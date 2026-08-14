4e249545533215fd2447c6d80c4aa0f276fffb95
Maximilian Maag <maagmaximilian@yourlocalcloud.de>
2026-08-14 16:00:19 +0200

    adding worker to PW config

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: 1,
+  workers: 10,
   reporter: [['html', { open: 'never' }], ['list']],
   use: {
     baseURL: process.env.E2E_BASE_URL ?? 'http://localhost:3000',
